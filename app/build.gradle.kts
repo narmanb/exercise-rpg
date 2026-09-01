eb437a3331420bf4ef8b15ab4a4fa102cd93cd48
narmanb <43427228+narmanb@users.noreply.github.com>
2026-08-31 21:30:14 -0500

    Add current Play services activity recognition dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.health.connect:connect-client:1.1.0")
+    implementation("com.google.android.gms:play-services-location:21.4.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 
