93f31da32c173001f95a88c4649483da4f64b43f
narmanb <43427228+narmanb@users.noreply.github.com>
2026-09-03 23:42:56 -0500

    Bump Oxford shadow test build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pathofthewild.game"
         minSdk = 28
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
